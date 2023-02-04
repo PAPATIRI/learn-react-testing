ff738b6ca95c61ff9f0ab539931172e943f8ef52
PAPATIRI <syariftaufik11@gmail.com>
2023-02-04 17:07:41 +0700

    tambah test pada link learn berdasarkan id test

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
@@ -11,9 +11,10 @@ function App() {
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://zanccode.vercell.app"
           target="_blank"
           rel="noopener noreferrer"
+          data-testid="learn-link"
         >
           Learn React
         </a>
